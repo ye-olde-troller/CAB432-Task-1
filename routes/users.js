55b1c0766ae961263401b03d62a5d42a991749ff
Ashar Cosgrove <yeoldetroller@gmail.com>
2023-09-10 00:38:51 +1000

    get popular game from most clipped

--- routes/users.js
+++ routes/users.js
@@ -20,17 +20,15 @@ router.get('/:userID', function(req, res, next) {
     console.log(data);
     //channels endpoint has the most recent game they played
     //can't currently find a way to get the game played in all their recent streams
-    axios.get(`https://api.twitch.tv/helix/channels?broadcaster_id=${data.id}`, {
-      headers: {
-        'Accept': 'application/json',
-        'Client-ID': process.env.CLIENT_ID,
-        'Authorization': "Bearer " + res.locals.API_TOKEN
-      },
+
+    getData(data.id, res.locals.API_TOKEN)
+    .then(count => {
+      console.log("count: ", count);
+      res.render('users', { title: 'Express', userID: req.params.userID, ...data});
     })
-    .then(response => {
-      console.log(response.data.data);
+    .catch(e => {
+      console.error(e);
     })
-    res.render('users', { title: 'Express', userID: req.params.userID, ...data});
 
   })
   .catch(e => {
@@ -38,5 +36,87 @@ router.get('/:userID', function(req, res, next) {
     next(createHttpError(500));
   });
 });
+
+function getData(userId, API_TOKEN){
+  return new Promise((resolve, reject) => {
+    axios.get(`https://api.twitch.tv/helix/clips?broadcaster_id=${userId}&first=100`, {
+      headers: {
+        'Accept': 'application/json',
+        'Client-ID': process.env.CLIENT_ID,
+        'Authorization': "Bearer " + API_TOKEN
+      },
+    })
+    .then(response => {
+      return response.data.data;
+    })
+    .then(data => {
+      var count = {};
+      for(entry in data){
+        count[data[entry].game_id] = count[data[entry].game_id] ? count[data[entry].game_id] + 1 : 1;
+      }
+
+      let entries = Object.entries(count);
+      let sorted = entries.sort((a, b) => b[1] - a[1]);
+      
+      games = []
+      for(item in sorted){
+        games.push(getGame(sorted[item][0], API_TOKEN));
+      }
+
+      Promise.allSettled(games).then(values => {
+        for(entry in values){
+          if(values[entry].status == "fulfilled"){
+            return values[entry].value;
+          }
+        }
+      })
+      .then(value => {
+        resolve(value);
+      })
+      .catch(e => {
+        console.error(e);
+      });
+    })
+    .catch(e => {
+      console.error(e);
+      reject(e);
+    })
+  });
+}
+
+function getGame(gameId, API_TOKEN){
+  return new Promise((resolve, reject) => {
+    var body = `
+      fields
+      name,
+      similar_games.name,
+      similar_games.id,
+      similar_games.cover.image_id,
+      similar_games.external_games.uid,
+      similar_games.external_games.category
+      ;
+      where external_games.uid = "${gameId}";  
+    `
+
+    axios.post("https://api.igdb.com/v4/games", body, {
+      headers: {
+        'Accept': 'application/json',
+        'Client-ID': process.env.CLIENT_ID,
+        'Authorization': "Bearer " + API_TOKEN
+      },
+    })
+    .then(response => {
+      return response.data;
+    })
+    .then(data => {
+      console.log(data);
+      resolve(data);
+    })
+    .catch(e => {
+      console.error(e);
+      reject(e);
+    })
+  })
+}
 
 module.exports = router;
